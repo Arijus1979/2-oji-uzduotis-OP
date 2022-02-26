2c08cf5eec8caf119af41c015ee7be3a1f10ddf9
Arijus1979 <arijusta@gmail.com>
2022-02-27 01:21:50 +0200

    Aptvarkymas

--- programa.cpp
+++ programa.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-struct stud{                //struktura studento duomenims saugoti
+struct stud {                //struktura studento duomenims saugoti
     string vardas;
     string pavarde;
     vector<int> nd;
@@ -11,18 +11,48 @@ struct stud{                //struktura studento duomenims saugoti
     float med;
 };
 
-int main()
+void skaicius(int& a) //funkcija neleidzianti ivesti raides ten kur reikia skaiciaus
 {
-    srand(time(0));
-    vector<stud> studentas;
+    cin >> a;
+    while (!cin)
+    {
+        cin.clear(); // reset failbit
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //praleidziame neteisinga ivesti
+        cout << "Prasome ivesti skaiciu: ";
+        cin >> a;
+    }
+}
+
+void skaiciavimai(vector<stud>& studentas, int j, int pazsum, int i) //skaiciavimo funkcijos
+{
+    float mediana;
+    sort(studentas[i].nd.begin(), studentas[i].nd.end());
+    studentas[i].nd.erase(studentas[i].nd.begin());
+    if (j == 1)
+        mediana = studentas[i].nd[j];
+    else if (j % 2 == 0) //medianos skaiciavimas jei nariu skaicius yra lyginis
+    {
+        mediana = (studentas[i].nd[j / 2] + studentas[i].nd[j / 2 - 1]) / 2.0;
+    }
+    else        //medianos skaiciavimas jei nariu skaicius nelyginis
+        mediana = (studentas[i].nd[j / 2]);
+
+    studentas[i].med = mediana;
+
+
+    studentas[i].gal = (0.4 * (pazsum / (float)j)) + (0.6 * studentas[i].egz);
+    studentas[i].med = (0.4 * (pazsum / (float)j)) + (0.6 * studentas[i].med);
+}
+
+void ivedimas(vector<stud>& studentas, int& i)
+{
     studentas.push_back(stud());
     string v, p;
-    int a=1;
-    float pazsum=0; // pazymiu suma
-    int i=0;
-    int j=0;
+    int a = 1;
+    float pazsum = 0; // pazymiu suma
+    int j = 0;
     int u;
-    while(true)
+    while (true)
     {
         cout << "Iveskite studento varda ir pavarde: ";
         cin >> v >> p;
@@ -30,49 +60,42 @@ int main()
         studentas[i].pavarde = p;
         char A;
         cout << "Ar norite atsitiktinai sugeneruoti mokinio namu darbu ir egzamino pazymius? (Y/N) ";
-        while(true)
+        while (true)
         {
             cin >> A;
             if (A != 'y' && A != 'n' && A != 'Y' && A != 'N')
                 cout << "Y/N ";
             else break;
         }
-        if(A == 'Y' || A == 'y')
+        if (A == 'Y' || A == 'y')
         {
             cout << "Iveskite norimu sugeneruoti pazymiu skaiciu: ";
-            cin >> u;
-            for(int p=0; p<u; p++)
+            skaicius(u);
+            for (int p = 0; p < u; p++)
             {
-                a = rand()%10+1;
+                a = rand() % 10 + 1;
                 studentas[i].nd.push_back(a);
-                pazsum += a;         
+                pazsum += a;
                 j++;
             }
             j++;
-            a = rand()%10+1;
+            a = rand() % 10 + 1;
             studentas[i].egz = a;
-            a=1;
+            a = 1;
         }
         else
         {
             cout << "Veskite studento pazymius, baige vesti, iveskite 0: ";
-            while(true)
+            while (true)
             {
 
-                if(a!=0)
+                if (a != 0)
                 {
-                    cin >> a;
-                    while(!cin)
-                    {
-                        cin.clear(); // reset failbit
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //praleidziame neteisinga ivesti
-                        cout << "Prasome ivesti skaiciu: ";
-                        cin >> a;
-                    }
-                    while(a<0 || a>10)
+                    skaicius(a);
+                    while (a < 0 || a>10)
                     {
                         cout << "Prasome ivesti pazymi intervale [1, 10]: ";
-                        cin >> a;
+                        skaicius(a);
                     }
                     studentas[i].nd.push_back(a);
                     pazsum += a;
@@ -80,74 +103,68 @@ int main()
                 }
                 else
                 {
-                    if(pazsum==0)
+                    if (pazsum == 0)
                     {
                         cout << "prasome ivesti bent viena pazymi: ";
-                        cin >> a;
+                        skaicius(a);
                         pazsum += a;
                     }
                     else
                         break;
                 }
-                    
+
             }
             cout << "Iveskite egzamino rezultata: ";
-            cin >> a;
-            while(a<0 || a>10)
+            skaicius(a);
+            while (a < 0 || a>10)
             {
                 cout << "Prasome ivesti pazymi intervale [0, 10]: ";
-                cin >> a;
+                skaicius(a);
             }
             studentas[i].egz = a;
-            a=1;
+            a = 1;
         }
 
         j--;
-        float mediana;
-        sort(studentas[i].nd.begin(), studentas[i].nd.end());
-        studentas[i].nd.erase(studentas[i].nd.begin());
-        
-        
-        if(j==1)
-        mediana=studentas[i].nd[j];
-        else if(j%2==0) //medianos skaiciavimas jei nariu skaicius yra lyginis
-        {
-            mediana=(studentas[i].nd[j/2]+studentas[i].nd[j/2 - 1])/2.0;
-        }
-        else        //medianos skaiciavimas jei nariu skaicius nelyginis
-            mediana=(studentas[i].nd[j/2]);
 
-        studentas[i].med = mediana;
-
-
-       
-        
-        studentas[i].gal = (0.4*(pazsum/(float)j)) + (0.6*studentas[i].egz);
-        studentas[i].med = (0.4*(pazsum/(float)j)) + (0.6*studentas[i].med);
+        skaiciavimai(studentas, j, pazsum, i);
 
         cout << "Ar norite ivesti kita mokini? (Y/N) ";
-        while(true)
+        while (true)
         {
             cin >> A;
             if (A != 'y' && A != 'n' && A != 'Y' && A != 'N')
                 cout << "Y/N ";
             else break;
         }
-        if(A == 'n' || A == 'N')
+        if (A == 'n' || A == 'N')
         {
             break;
         }
-        j=0;
+        j = 0;
         i++;
-        pazsum=0;
+        pazsum = 0;
         studentas.push_back(stud());
     }
+}
 
-
-    cout << "|"<< left << setw(20) << "Vardas" << "|" << left << setw(20) << "Pavarde" << "|" << left << setw(20) << "Galutinis (Vid.)"<< "|" << left << setw(20) << "Galutinis (Med.)" << endl;
-    for(int k=0; k <= i; k++)
+void isvedimas(int i, vector<stud> studentas)  //duomenu isvedimo funkcija
+{
+    cout << "|" << left << setw(20) << "Vardas" << "|" << left << setw(20) << "Pavarde" << "|" << left << setw(20) << "Galutinis (Vid.)" << "|" << left << setw(20) << "Galutinis (Med.)" << endl;
+    for (int k = 0; k <= i; k++)
     {
-        cout << "|"<< left << setw(20) << studentas[k].vardas << "|" << left << setw(20) << studentas[k].pavarde << "|" << left << setw(20) << fixed << setprecision(2) << studentas[k].gal << "|" << left << setw(20) << studentas[k].med << endl;;
+        cout << "|" << left << setw(20) << studentas[k].vardas << "|" << left << setw(20) << studentas[k].pavarde << "|" << left << setw(20) << fixed << setprecision(2) << studentas[k].gal << "|" << left << setw(20) << studentas[k].med << endl;;
     }
+}
+
+int main()
+{
+    srand(time(0));
+    vector<stud> studentas;
+    int i = 0;
+
+    ivedimas(studentas, i);
+
+    isvedimas(i, studentas);
 
 }
