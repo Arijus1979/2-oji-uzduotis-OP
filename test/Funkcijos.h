c93ee76ab5d729392051880ba9434fe07b7c8d3b
Arijus1979 <arijusta@gmail.com>
2022-03-06 13:13:45 +0200

    Sukurti headers failai

--- test/Funkcijos.h
+++ test/Funkcijos.h
@@ -1,37 +1,12 @@
-#include <bits/stdc++.h>
+#pragma once
 
+#include "Strukturos.h"
 
 using namespace std;
+ifstream in("kursiokai.txt");
 
-struct stud {                //struktura studento duomenims saugoti
-    string vardas;
-    string pavarde;
-    int kiekis;
-    int* nd = new int[2];
-    float egz;
-    float gal;
-    float med;
-};
+bool skaitymasisfailo = false;
 
-void resize(stud* studentas, int& size) 
-{
-    stud* resize_arr = new stud[size + 1];
-    for (int i = 0; i < size; i++)
-        resize_arr[i] = studentas[i];
-    size++;
-    studentas = resize_arr;
-    delete[] resize_arr;
-}
-
-void resize_nd(int* studentas, int& size) 
-{
-    int* resize_arr = new int[size + 1];
-    for (int i = 0; i < size; i++)
-        resize_arr[i] = studentas[i];
-    size++;
-    studentas = resize_arr;
-    delete[] resize_arr;
-}
 
 
 void skaicius(int& a) //funkcija neleidzianti ivesti raides ten kur reikia skaiciaus
@@ -46,17 +21,16 @@ void skaicius(int& a) //funkcija neleidzianti ivesti raides ten kur reikia skaic
     }
 }
 
-void skaiciavimai(stud* studentas, int j, int pazsum, int i) //skaiciavimo funkcijos
+void skaiciavimai(vector<stud>& studentas, int j, int pazsum, int i) //skaiciavimo funkcijos
 {
     float mediana;
-    
-    sort(studentas[i].nd, studentas[i].nd + j);
-
+    sort(studentas[i].nd.begin(), studentas[i].nd.end());
+    studentas[i].nd.erase(studentas[i].nd.begin());
     if (j == 1)
         mediana = studentas[i].nd[j];
     else if (j % 2 == 0) //medianos skaiciavimas jei nariu skaicius yra lyginis
     {
-        mediana = (studentas[i].nd[j/2] + studentas[i].nd[j/2 - 1]) / 2.0;
+        mediana = (studentas[i].nd[j / 2] + studentas[i].nd[j / 2 - 1]) / 2.0;
     }
     else        //medianos skaiciavimas jei nariu skaicius nelyginis
         mediana = (studentas[i].nd[j / 2]);
@@ -68,20 +42,22 @@ void skaiciavimai(stud* studentas, int j, int pazsum, int i) //skaiciavimo funkc
     studentas[i].med = (0.4 * (pazsum / (float)j)) + (0.6 * studentas[i].med);
 }
 
-void ivedimas(stud* studentas, int& i)
+void ivedimas(vector<stud>& studentas, int& i)
 {
+    studentas.push_back(stud());
     string v, p;
     int a = 1;
     float pazsum = 0; // pazymiu suma
     int j = 0;
     int u;
+    char A;
+
     while (true)
     {
         cout << "Iveskite studento varda ir pavarde: ";
         cin >> v >> p;
         studentas[i].vardas = v;
         studentas[i].pavarde = p;
-        char A;
         cout << "Ar norite atsitiktinai sugeneruoti mokinio namu darbu ir egzamino pazymius? (Y/N) ";
         while (true)
         {
@@ -94,13 +70,11 @@ void ivedimas(stud* studentas, int& i)
         {
             cout << "Iveskite norimu sugeneruoti pazymiu skaiciu: ";
             skaicius(u);
-            studentas[i].kiekis = u;
-            resize_nd(studentas[i].nd, studentas[i].kiekis);
             for (int p = 0; p < u; p++)
             {
                 a = rand() % 10 + 1;
-                studentas[i].nd[p] = a;
-                pazsum += a;d
+                studentas[i].nd.push_back(a);
+                pazsum += a;
                 j++;
             }
             j++;
@@ -122,8 +96,7 @@ void ivedimas(stud* studentas, int& i)
                         cout << "Prasome ivesti pazymi intervale [1, 10]: ";
                         skaicius(a);
                     }
-                    studentas[i].nd[j] = a;
-                    resize_nd(studentas[i].nd, j);
+                    studentas[i].nd.push_back(a);
                     pazsum += a;
                     j++;
                 }
@@ -168,31 +141,101 @@ void ivedimas(stud* studentas, int& i)
             break;
         }
         j = 0;
-        resize(studentas, i);
-        
-        
+        i++;
         pazsum = 0;
-        
+        studentas.push_back(stud());
     }
 }
 
-void isvedimas(int i, stud* studentas)  //duomenu isvedimo funkcija
+bool rusiavimasvard(stud& a, stud& b)
 {
-    cout << "|" << left << setw(20) << "Vardas" << "|" << left << setw(20) << "Pavarde" << "|" << left << setw(20) << "Galutinis (Vid.)" << "|" << left << setw(20) << "Galutinis (Med.)" << endl;
-    for (int k = 0; k <= i; k++)
+    return a.vardas < b.vardas;
+}
+
+bool rusiavimaspavard(stud& a, stud& b)
+{
+    return a.pavarde < b.pavarde;
+}
+
+void isvedimas(int i, vector<stud> studentas)  //duomenu isvedimo funkcija
+{
+    char A;
+    cout << "Ar rezultatus norite rusiuoti pagal varda ar pavarde? [V/P] ";
+    while (true)
     {
-        cout << "|" << left << setw(20) << studentas[k].vardas << "|" << left << setw(20) << studentas[k].pavarde << "|" << left << setw(20) << fixed << setprecision(2) << studentas[k].gal << "|" << left << setw(20) << studentas[k].med << endl;
+        cin >> A;
+        if (A != 'v' && A != 'p' && A != 'V' && A != 'P')
+            cout << "V/P ";
+        else break;
     }
+    if (A == 'V' || A == 'v')
+    {
+        sort(studentas.begin(), studentas.end(), rusiavimasvard);
+    }
+    else
+        sort(studentas.begin(), studentas.end(), rusiavimaspavard);
+
+
+    cout << "|" << left << setw(20) << "Vardas" << "|" << left << setw(20) << "Pavarde" << "|" << left << setw(20) << "Galutinis (Vid.)" << "|" << left << setw(20) << "Galutinis (Med.)" << endl;
+
+    if (skaitymasisfailo == false)
+    {
+        for (int k = 0; k <= i; k++)
+        {
+            cout << "|" << left << setw(20) << studentas[k].vardas << "|" << left << setw(20) << studentas[k].pavarde << "|" << left << setw(20) << fixed << setprecision(2) << studentas[k].gal << "|" << left << setw(20) << studentas[k].med << endl;;
+        }
+    }
+    else
+    {
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "|" << left << setw(20) << studentas[k].vardas << "|" << left << setw(20) << studentas[k].pavarde << "|" << left << setw(20) << fixed << setprecision(2) << studentas[k].gal << "|" << left << setw(20) << studentas[k].med << endl;;
+        }
+    }
+
+
 }
 
-int main()
+int ndkiekis()
 {
-    srand(time(0));
-    stud* studentas = new stud[4];
-    int i = 0;
 
-    ivedimas(studentas, i);
+    string a;
+    int q = 0;
+    while (true)
+    {
+        in >> a;
+        q++;
+        if (a == "Egz.")
+        {
+            break;
+        }
 
-    isvedimas(i, studentas);
+    }
+    return q - 3;
+}
 
+void nuskaitymas(vector<stud>& studentas, int& i)
+{
+    float pazsum = 0; // pazymiu suma
+    int ndpaz;
+    int kieknd = ndkiekis();
+    studentas.push_back(stud());
+    while (!in.eof())
+    {
+        in >> studentas[i].vardas >> studentas[i].pavarde;
+        for (int q = 0; q < kieknd; q++)
+        {
+            in >> ndpaz;
+            studentas[i].nd.push_back(ndpaz);
+            pazsum += ndpaz;
+        }
+        in >> studentas[i].egz;
+
+        skaiciavimai(studentas, kieknd, pazsum, i);
+
+        i++;
+        pazsum = 0;
+        studentas.push_back(stud());
+    }
+    skaitymasisfailo = true;
 }
---
+++ test/Strukturos.h
@@ -0,0 +1,18 @@
+#pragma once
+
+using namespace std;
+ifstream in("kursiokai.txt");
+
+bool skaitymasisfailo = false;
+
+
+
+
+struct stud {                //struktura studento duomenims saugoti
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    float egz;
+    float gal;
+    float med;
+};
